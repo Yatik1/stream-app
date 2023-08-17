4dd3739c8c84c2a98f8af77f224bb15d7ad75e49
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-08-17 17:56:10 +0530

    ::carousel --> implementation

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
+import SwitchTab from '../../../components/switchTab/SwitchTab';
+
+import "./Trending.scss"
+
 
 const Trending = () => {
+
+  const onTabChange = (tab) => {};
+
   return (
-    <div>
-       Trending Moviess 
-    </div>
+     <div className="carouselSection">
+       <ContentWrapper>
+         <span className="carouselTitle">
+           Trending
+         </span>
+         <SwitchTab data={["Day" , "Week"]} onTabChange={onTabChange}/>
+       </ContentWrapper>
+     </div>
   )
 }
 
