d3ebf4d7b160888f0f65fb5346c27cda4951ee7e
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-08-19 22:53:29 +0530

    ::Carousel implemented

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -21,7 +21,18 @@ const Carousel = ({data,loading}) => {
   const {url} = useSelector((state) => state.home)
   const navigate = useNavigate()
     
-  const navigation = (dir) => { }
+  // console.log(carouselContainer)
+  const navigation = (dir) => { 
+     const container = carouselContainer.current;
+
+     const scrollAmount = dir === "left" ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth+20);
+
+     container.scrollTo({
+       left: scrollAmount ,
+       behavior : "smooth", 
+     })
+      
+  }
   
   const skItem = () => {
     return (
@@ -48,12 +59,12 @@ const Carousel = ({data,loading}) => {
                 onClick={() => navigation("right")}/>
 
         {!loading ? (
-           <div className="carouselItems">
+           <div className="carouselItems" ref={carouselContainer}>
              {data?.map((item) => {
                 const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
 
                 return (
-                    <div key={item.id} className="carouselItem">
+                    <div key={item.id} className="carouselItem" onClick={() => navigate(`/${item.media_type}/${item.id}`)}>
                         <div className="posterBlock">
                            <Img src={posterUrl}/>   
                            <Rating rating={item.vote_average.toFixed(1)}/> 
