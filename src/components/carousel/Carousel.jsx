6b671ed50467042d4dbf7dc9b5b2cd9564c49895
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-09-12 17:29:04 +0530

    ::recommendation section

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,7 +15,7 @@ import "./carousel.scss"
 import Rating from "../rating/Rating";
 import Genres from "../genres/Genres";
 
-const Carousel = ({data,loading,endpoint}) => { 
+const Carousel = ({data,loading,endpoint,title}) => { 
 
   const carouselContainer = useRef()
   const {url} = useSelector((state) => state.home)
@@ -49,6 +49,7 @@ const Carousel = ({data,loading,endpoint}) => {
   return (
     <div className="carousel">
         <ContentWrapper>
+           {title && <div className="carouselTitle">{title}</div>}
             <BsFillArrowLeftCircleFill 
                 className="carouselLeftNav arrow" 
                 onClick={() => navigation("left")} 
