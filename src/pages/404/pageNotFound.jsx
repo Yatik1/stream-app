f96ff0d5c926dec94b994959f292f3d0a6d79f30
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-22 00:16:55 +0530

    not found page

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function PageNotFound() {
-  return (
-    <div>
-      Page not found
-    </div>
-  )
-}
+import "./pageNotFound.scss";
 
-export default PageNotFound
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
